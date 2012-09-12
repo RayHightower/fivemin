21090394d2710bf9b0952b471877db1750cef37e
Raymond T. Hightower <rth@wisdomgroup.com>
2012-09-12 17:35:20 -0500

    playing with app_delegate.rb

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,14 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
+
+    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10, 10, 100, 100))
+    @blue_view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@blue_view)
+
+    puts "Hello again in the console!"
+
     true
   end
 end
