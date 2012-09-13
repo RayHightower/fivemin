bfbb3ae36ed1a77a641731c90d8b5e9d5649d80d
Raymond T. Hightower <rth@wisdomgroup.com>
2012-09-12 19:58:02 -0500

    now this one works too. watch your hashes, RTH!!!

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,21 +3,13 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
-    # controller = TapController.alloc.initWithNibName(nil, bundle: nil)
-    # nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
-
-    # tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle:nil)
-    # tab_controller.viewControllers = (nav_controller)
-
-    # @window.rootViewController = tab_controller
-    
     controller = TapController.alloc.initWithNibName(nil, bundle: nil)
     nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
 
-    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle:nil)
     tab_controller.viewControllers = [nav_controller]
-    @window.rootViewController = tab_controller
 
+    @window.rootViewController = tab_controller
 
     puts "Hello again in the console!"
 
