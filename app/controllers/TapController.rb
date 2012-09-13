086d45ce2d174dc116dafd113bfad0e907e17ccd
Raymond T. Hightower <rth@wisdomgroup.com>
2012-09-12 19:11:27 -0500

    change things with every demo. cool way to learn

--- app/controllers/TapController.rb
+++ app/controllers/TapController.rb
@@ -2,7 +2,13 @@ class TapController < UIViewController
   def viewDidLoad
     super
 
-    self.view.backgroundColor = UIColor.redColor
+    self.view.backgroundColor = UIColor.whiteColor
+
+    @label = UILabel.alloc.initWithFrame(CGRectZero)
+    @label.text = "Let's just change this at random."
+    @label.sizeToFit
+    @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
+    self.view.addSubview @label
     
   end
 end
