03a486f8c55a2cc6899e5bc2c413c2854c32881e
Raymond T. Hightower <rth@wisdomgroup.com>
2012-09-12 19:55:19 -0500

    this one works. why?

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,14 +3,22 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
-    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
-    nav_controller = UINavigationController.alloc.initWIthRootViewController(controller)
+    # controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    # nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
 
-    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle:nil)
-    tab_controller.viewControllers = (nav_controller)
+    # tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle:nil)
+    # tab_controller.viewControllers = (nav_controller)
 
-    @window.rootViewController = tab_controller
+    # @window.rootViewController = tab_controller
     
+    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [nav_controller]
+    @window.rootViewController = tab_controller
+
+
     puts "Hello again in the console!"
 
     true
