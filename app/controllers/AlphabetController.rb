743084e97627b38eab33bc3e711e13a261597fc3
Raymond T. Hightower <rth@wisdomgroup.com>
2012-09-13 00:11:12 -0500

    now to get the data source working

--- app/controllers/AlphabetController.rb
+++ app/controllers/AlphabetController.rb
@@ -9,5 +9,31 @@ class AlphabetController < UIViewController
     @table = UITableView.alloc.initWithFrame(self.view.bounds)
     self.view.addSubview @table
    
+    @table.datasource = self
+
+    @data["A".."Z"].to_a
+
   end
+
+  def tableView(tableView, cellForrowAtIndexPath: indexPath)
+    # return the UITableViewCell for the row
+
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableVeiwCellStyleDefault, reuseIdenfier:@reuseIdentifier)
+    end
+    
+    cell.textLabel.text = @data[indexPath.row]
+
+    cell
+
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    # return the number of rows
+    @data.count
+
+  end
+
 end
