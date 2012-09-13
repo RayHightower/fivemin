64dbf2b69de44222a7d0327c195124c6f7828e31
Raymond T. Hightower <rth@wisdomgroup.com>
2012-09-13 01:16:11 -0500

    runs correctly with tableView

--- app/controllers/AlphabetController.rb
+++ app/controllers/AlphabetController.rb
@@ -9,32 +9,32 @@ class AlphabetController < UIViewController
     @table = UITableView.alloc.initWithFrame(self.view.bounds)
     self.view.addSubview @table
    
-#     @table.dataSource = self
-#     @table.delegate = self
-# 
-#     @data = ("A".."Z").to_a
+    @table.dataSource = self
+    @table.delegate = self
+ 
+    @data = ("A".."Z").to_a
 
   end
 
-#   def tableView(tableView, cellForrowAtIndexPath: indexPath)
-#     # return the UITableViewCell for the row
-# 
-#     @reuseIdentifier ||= "CELL_IDENTIFIER"
-# 
-#     cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
-#       UITableViewCell.alloc.initWithStyle(UITableVeiwCellStyleDefault, reuseIdenfier:@reuseIdentifier)
-#     end
-#     
-#     cell.textLabel.text = @data[indexPath.row]
-# 
-#     cell
-# 
-#   end
+  def tableView(tableView, cellForRowAtIndexPath: indexPath)
+    # return the UITableViewCell for the row
 
-#   def tableView(tableView, numberOfRowsInSection: section)
-#     # return the number of rows
-#     @data.count
-# 
-#   end
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    end
+    
+    cell.textLabel.text = @data[indexPath.row]
+
+    cell
+
+  end
+
+  def tableView(tableView, numberOfRowsInSection: section)
+    # return the number of rows
+    @data.count
+
+  end
 
 end
